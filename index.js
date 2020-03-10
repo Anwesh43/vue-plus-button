6c7cf26670086710c0c938cdea6c71b88abf8983
Anwesh43 <anweshthecool0@gmail.com>
2020-03-10 09:03:32 +0530

    adding plus button component

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const scGap = 0.02
 const delay = 30
+const radiusFactor = 5
+const sizeFactor = 8
 
 class AnimatedScale {
 
@@ -12,7 +14,7 @@ class AnimatedScale {
             this.animated = true
             setInterval(() => {
                 scale += scGap
-                cb(scale)
+                cb(Math.sin(scale * Math.PI))
             }, delay)
         }
     }
@@ -24,3 +26,49 @@ class AnimatedScale {
         }
     }
 }
+
+Vue.component('plus-button', {
+    data() {
+        const sf = 0
+        const animatedScale = new AnimatedScale()
+        return {sf, animatedScale}
+    },
+    methods: {
+        start() {
+            animatedScale.start((sf) => {
+                this.sf = sf
+            })
+        }
+    },
+    computed: {
+        objStyle() {
+            const size = Math.min(w, h) / radiusFactor
+            const position = 'absolute'
+            const left = `${w / 2}px`
+            const top = `${h / 2}px`
+            const width = `${size}px`
+            const height = `${size}px`
+            const borderRadius = '50%'
+            const background = '#4CAF50'
+            const WebkitTransform = `rotate(${45 * this.sf}deg)`
+            return {position, left, top, width, height, background, borderRadius, WebkitTransform}
+        },
+        plusStyle1() {
+            const size = Math.min(w, h) / sizeFactor
+            const width = `${size}px`
+            const height = `${size / 11}px`
+            const background = 'white'
+            const position = 'absolute'
+            const left = `${-size / 2}px`
+            const top = `${-parseFloat(height) / 2}px`
+            return  {width, height, background, position, top, left}
+        },
+
+        plusStyle2() {
+            const style = Object.assign({}, this.plusStyle1())
+            style.WebkitTransform = `rotate(90deg)`
+            return style
+        }
+    },
+    template : '<div :style = "objStyle" @click = "start"><div :style = "plusStyle1"></div><div :style = "plusStyle2"></div></div>'
+})
